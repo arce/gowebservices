b7989535113ef21dba80dfb987418cce6f747d73
Armando Arce <armando.arce@gmail.com>
2020-07-27 20:16:19 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-)
+  "fmt"
+  "net/http" 
+  "os")
+  
+func handler(writer http.ResponseWriter, request *http.Request) { 
+	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")
-	})
-
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+  http.HandleFunc("/", handler) http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
