02b464df5c0d45b8bb092fcaa5cdb53979cf3b53
Armando Arce <armando.arce@gmail.com>
2020-10-27 19:03:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once(“home.html”); ?>
+<html>
+<head>
+  <script type="text/javascript" src="js/fetchival.min.js"></script>
+  <script type="text/javascript" src="app.js"></script>
+</head>
+<body>
+  <script>
+	  test();
+	  alert("This is alert box!");
+  </script>
+</body>
+</html>
