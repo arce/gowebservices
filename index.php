b7c23c4aaa5adad0b8544782dcf57ffc6028e37e
Armando Arce <armando.arce@gmail.com>
2023-07-24 15:18:07 -0600

    Update

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<?php 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-?>
 <html>
 <head>
-  <script type="text/javascript" src="js/fetchival.min.js"></script>
+  <script type="text/javascript" src="data.json"></script>
+  <script type="text/javascript" src="js/mustache.min.js"></script>
   <script type="text/javascript" src="app.js"></script>
 </head>
 <body>
